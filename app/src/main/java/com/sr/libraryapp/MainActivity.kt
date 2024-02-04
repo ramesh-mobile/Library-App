85488f1780dd8b6a00cf605efca63c1c80983b62
ramesh <ramesh007chauhan@gmail.com>
2024-02-04 15:46:13 +0530

    added geofencing demo

--- app/src/main/java/com/sr/libraryapp/MainActivity.kt
+++ app/src/main/java/com/sr/libraryapp/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.sr.libraryapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.sr.libraryapp.databinding.ActivityMainBinding
+import com.sr.libraryapp.location.LocationModule
+import com.sr.libraryapp.utils.getLocationString
+import com.sr.libraryapp.utils.isInRadius
 import com.sr.libraryapp.utils.logPrint
 
 class MainActivity : AppCompatActivity() {
@@ -15,22 +20,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        GoogleLocationModule.init(this)
-        //getLocation()
+        //GoogleLocationModule.init(this)
+        LocationModule.init(this)
         binding.tvAppName.setOnClickListener {
             getLocation()
         }
     }
 
     fun getLocation(){
-        /*LocationModule(this).getLocation{
-            logPrint(TAG,it)
-            binding.tvAppName.text = it
-        }*/
-
-        GoogleLocationModule().getLocation(isRepeated = true){
-            logPrint(TAG,it)
-            binding.tvAppName.text = it
+        var timeInterval : Long = 10000
+        LocationModule.getLocation(true, timeInterval){
+            onLocationFoundOperation(it)
         }
+        /*GoogleLocationModule.getLocation(isRepeated = true, timeInterval){
+            onLocationFoundOperation()
+        }*/
+    }
+
+    fun onLocationFoundOperation(it:Location){
+        var apiLocation = Location(LocationManager.NETWORK_PROVIDER)
+        apiLocation.latitude =18.969063515099037
+        apiLocation.longitude = 72.81220707268889
+
+        Log.d(TAG, "onLocationChanged: ${it.getLocationString()}")
+        binding.tvAppName.text = it.getLocationString()
+        logPrint(TAG, "getLocation: "+apiLocation.isInRadius(it,9.9f))
     }
 }
